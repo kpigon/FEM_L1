7e3df50f2d1acc24f19a380cadec062ba9dc8552
kpigon <devo4050@gmail.com>
2021-04-08 16:18:54 +0200

    hfghf

--- FEM1/FEM1.py
+++ FEM1/FEM1.py
@@ -1,5 +1,17 @@
 import numpy as np
 
-arr = np.array([1,2,3,4,5])
-print(arr)
-
+#arr = np.array([1,2,3,4,5])
+#print(arr)
+#A = np.array([[1,2,3],[7,8,9]])
+#print(A)
+#A = np.array([[1,2,3],
+#              [7,8,9]])
+#print(A)
+#A = np.array([[1,2, \
+#    3],
+#              [7,8,9]])
+#print(A)
+v = np.arange(1,7)
+print(v,"\n")
+v = np.arange(-2,7)
+print(v,"\n")
